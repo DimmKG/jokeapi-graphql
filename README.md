549391f17f64c77b829f82068c5bfb095df6d9dd
Dmitry Petrov <dimakrm361@gmail.com>
2021-10-25 17:37:51 +0700

    project: init

---
+++ README.md
@@ -0,0 +1,36 @@
+# JokeAPI GraphQL
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Running the app
+
+```bash
+# if your default nodeJS version is not v14.17.1
+$ nvm use 
+
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+$ npm run test
+
+# e2e tests
+$ npm run test:e2e
+
+# test coverage
+$ npm run test:cov
+```
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,51 @@
+import { Logger } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import {
+    TypeOrmModuleAsyncOptions,
+    TypeOrmModuleOptions,
+} from '@nestjs/typeorm'
+
+const DEFAULT_PSQL_HOST = 'localhost'
+const DEFAULT_PSQL_PORT = 5432
+
+export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
+    imports: [ConfigModule],
+    useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+        const logger = new Logger('TypeORM Configuration')
+        const database = configService.get<string>('PSQL_DATABASE')
+
+        if (database === undefined) {
+            throw new Error(
+                "Environment variable 'PSQL_DATABASE' cannot be undefined",
+            )
+        }
+
+        let syncMode = false
+        let dropDB = false
+
+        const nodeEnv = configService.get<string>('NODE_ENV')
+        if (nodeEnv === 'development') {
+            syncMode = Boolean(+configService.get('SYNC_MODE'))
+            logger.warn('Synchronization mode is activated')
+            if (syncMode) {
+                logger.warn(
+                    'Drop database mode is active. All saved data will be deleted!',
+                )
+                dropDB = Boolean(+configService.get('DROP_DATABASE'))
+            }
+        }
+
+        return {
+            type: 'postgres',
+            entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
+            database,
+            host: configService.get('PSQL_HOST') || DEFAULT_PSQL_HOST,
+            port: configService.get('PSQL_PORT') || DEFAULT_PSQL_PORT,
+            username: configService.get('PSQL_USERNAME'),
+            password: configService.get('PSQL_PASSWORD'),
+            synchronize: syncMode,
+            dropSchema: dropDB,
+        }
+    },
+    inject: [ConfigService],
+}
