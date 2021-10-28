dcec7ad351265996d70eb40c6507e67ffeefde70
Dmitry Petrov <dimakrm361@gmail.com>
2021-10-28 18:31:20 +0700

    refactor(jokes): move joke.resolver.ts to module's root

--- src/jokes/joke.resolver.ts
+++ src/jokes/joke.resolver.ts
@@ -3,11 +3,11 @@ import { Args, Int, Query, Resolver } from '@nestjs/graphql'
 
 import { AuthGuard } from 'src/auth/guards/auth.guard'
 
-import { Joke } from '../interfaces/joke.interfaces'
-import { JokesService } from '../jokes.service'
+import { JokeFiltersInput } from './graphql/types/joke.inputs'
+import { JokeType } from './graphql/types/joke.types'
+import { Joke } from './interfaces/joke.interfaces'
+import { JokesService } from './jokes.service'
 
-import { JokeFiltersInput } from './types/joke.inputs'
-import { JokeType } from './types/joke.types'
 @Resolver(() => JokeType)
 export class JokesResolver {
     constructor(private readonly _jokesService: JokesService) {}
--- src/jokes/jokes.module.ts
+++ src/jokes/jokes.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 
 import { AuthModule } from 'src/auth/auth.module'
 
-import { JokesResolver } from './graphql/joke.resolver'
+import { JokesResolver } from './joke.resolver'
 import { JokesService } from './jokes.service'
 
 @Module({
